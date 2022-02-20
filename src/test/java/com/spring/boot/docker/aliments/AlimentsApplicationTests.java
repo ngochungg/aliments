4b98f0487b214455b0df0affe8266396c4836bd7
Ngoc Hung <71111653+ngochungg@users.noreply.github.com>
2022-02-21 01:54:16 +0700

    alo

---
+++ src/test/java/com/spring/boot/docker/aliments/AlimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.docker.aliments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlimentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
